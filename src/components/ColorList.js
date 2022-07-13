8c42673854dc46eb913b187743bb506045f8d647
Ezra-code <njerukithinji872@gmail.com>
2022-07-13 19:01:48 +0300

    working with keys and lists

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+
+function ColorItem(props) { 
+  return <li style={{ color: props.color }}>{props.color}</li>
+}
+
 function ColorList() {
   const colors = [
     "firebrick",
@@ -7,20 +12,28 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "orange",
+    "blue",
   ];
 
+//   const colorElements = colors.map((color) => {
+//     return <li key={color} style={{ color: color }}>{color}</li>
+//   });
+  
+ const colorElements = colors.map((color) => { 
+    return <ColorItem key={ color} color={color} />;
+ })
+  
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorElements}
       </ol>
     </div>
   );
-}
+  
+ 
+ }
 
 export default ColorList;
